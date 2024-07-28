8a262e6d2b0c1b9552a470b589c749180f5594d1
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-07-28 19:55:05 +0530

    added check for portal

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,16 +1,22 @@
 'use client';
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 // Portal component to render element as child of body
 const Portal = ({children}:{children: ReactNode}) => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    },[])
+
     return(
         <>
-            {
+            {mounted?
                 createPortal(
                     children,
                     document.body
-                )
+                ):null
             }
         </>
     )
