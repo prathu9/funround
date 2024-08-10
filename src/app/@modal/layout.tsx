368b4606be1b3adeb1f99bea7b0db504ae287664
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-08-10 11:28:32 +0530

    fixed parallel route

--- src/app/@modal/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import {usePathname} from "next/navigation";
-import { ReactNode } from "react";
-
-export default function Layout({children}:{children: ReactNode}) {
-  const pathname = usePathname();
-  return pathname.startsWith("/signup") || pathname.startsWith("/login") || pathname.startsWith("/wallet-setup") ||  pathname.startsWith("/terms-of-use") || pathname.startsWith("/profile") ? children: null;
-}
--- src/app/@modal/signup/page.tsx
+++ src/app/@modal/signup/page.tsx
@@ -1,13 +1,16 @@
 import SignUp from "@/components/auth/SignUp";
 import ModalOverlay from "@/components/layout/ModalOverlay";
 import Portal from "@/components/layout/Portal";
-
+import Spinner from "@/components/layout/Spinner";
+import { Suspense } from "react";
 
 const Page = () => {
   return (
     <Portal>
       <ModalOverlay>
-        <SignUp/>
+        <Suspense fallback={<Spinner />}>
+          <SignUp />
+        </Suspense>
       </ModalOverlay>
     </Portal>
   );
